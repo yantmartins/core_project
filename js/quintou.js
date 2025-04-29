31e8393ae107ef52b4abeebe041f032f07ba4083
Yan <yantmartins@live.com>
2025-04-29 14:53:55 -0400

    finalizando js

--- js/quintou.js
+++ js/quintou.js
@@ -1,2 +1,30 @@
 let cadastrar = document.getElementById('cadastrar')
 let erros = document.getElementById('erros')
+
+cadastrar.addEventListener('click', async function (event){
+    event.preventDefault()
+
+    const formulario = document.getElementById('formulario');
+
+    const formData = new FormData(formulario);
+
+    let dados_php = await fetch('./actions/cad_prod.php',{
+        method: 'POST',
+        body:formData
+    });
+    let response = await dados_php.json()
+
+    if(response['error'] == false){
+        const modal = document.getElementById('modal')
+        modal.classList.remove('oculta')
+        modal.classList.add('chama')
+    }
+    else{
+        error = ''
+        Object.keys(response).forEach(erro => {
+            error += response[erro]
+            erros.innerText = error
+        })
+    }
+})
+
